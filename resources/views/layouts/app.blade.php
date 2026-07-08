81e1556f63dc411a2c6e883de8a2c8747b9ba824
Rizkha Tegar <rizkhategar@gmail.com>
2026-07-08 08:27:47 +0700

    fix(layout): remove laravel from browser title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'SIPENO') }} - Sistem Pengajuan Nomor Surat</title>
+    <title>SIPENO - Sistem Pengajuan Nomor Surat</title>
     <link rel="icon" type="image/png" href="{{ asset('images/logo.png') }}">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700&display=swap" rel="stylesheet" />
